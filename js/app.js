d5394b92d004c8cb2ec7543c93f2b309ba45db01
katiepestotnik <katie.pestotnik@gmail.com>
2023-10-04 14:04:17 -0600

    add exercise

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// 1. Select the button and cache  to a variable named btn.
+
+
+// 2. Select the input and cache to a variable named userInput.
+
+// 3. Select the div with a class of coding-topics and cache to a variable named codingTopics.
+
+
+// 4. Use the below function to write code that allows the user to add additional topics to the coding-topics div.
+const addTopic = (e) => {
+    //create new div named NewTopic 
+
+    //set the text of newTopic to the value added by the user
+
+    //add newTopic to codingTopics
+
+}
+// 5. Write an click eventListener for the button using the addTopic() function. 
+
+
+
+// 6. Select all the divs inside coding-topics and cache to a variable named allTopics 
+
+
+//7. Loop through allTopics and add a class named topic
